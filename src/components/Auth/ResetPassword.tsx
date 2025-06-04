1d276b8bce7e0c40cf41fedad7ca3db47820cf19
AdityaJ2305 <adityajindal2305@gmail.com>
2025-06-05 01:25:59 +0530

    refactor

--- src/components/Auth/ResetPassword.tsx
+++ src/components/Auth/ResetPassword.tsx
@@ -1,12 +1,26 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { navigate } from "raviger";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 import { toast } from "sonner";
+import * as z from "zod";
+
+import { cn } from "@/lib/utils";
 
 import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { PasswordInput } from "@/components/ui/input-password";
 
+import Loading from "@/components/Common/Loading";
 import { ValidationHelper } from "@/components/Users/UserFormValidations";
 
 import { LocalStorageKeys } from "@/common/constants";
@@ -16,184 +30,161 @@ import routes from "@/Utils/request/api";
 import mutate from "@/Utils/request/mutate";
 import query from "@/Utils/request/query";
 
-interface ResetPasswordProps {
-  token: string;
-}
-
-const ResetPassword = (props: ResetPasswordProps) => {
-  const initForm: any = {
-    password: "",
-    confirm: "",
-  };
-
-  const initErr: any = {};
-  const [form, setForm] = useState(initForm);
-  const [errors, setErrors] = useState(initErr);
-  const [isPasswordFieldFocused, setIsPasswordFieldFocused] = useState(false);
-
+export default function ResetPassword({ token }: { token: string }) {
   const { t } = useTranslation();
-  const handleChange = (e: any) => {
-    const { value, name } = e.target;
-    const fieldValue = Object.assign({}, form);
-    const errorField = Object.assign({}, errors);
-    if (errorField[name]) {
-      errorField[name] = null;
-      setErrors(errorField);
-    }
-    fieldValue[name] = value;
-    setForm(fieldValue);
-  };
 
-  const validateData = () => {
-    let hasError = false;
-    const err = Object.assign({}, errors);
-    if (form.password !== form.confirm) {
-      hasError = true;
-      err.confirm = t("password_mismatch");
-    }
-
-    if (!validatePassword(form.password)) {
-      hasError = true;
-      err.password = t("invalid_password");
-    }
-
-    Object.keys(form).forEach((key) => {
-      if (!form[key]) {
-        hasError = true;
-        err[key] = t("field_required");
-      }
+  const resetPasswordFormSchema = z
+    .object({
+      password: z.string().refine((val) => validatePassword(val), {
+        message: t("new_password_validation"),
+      }),
+      confirm_password: z.string(),
+    })
+    .refine((data) => data.password === data.confirm_password, {
+      message: t("password_mismatch"),
+      path: ["confirm_password"],
     });
-    if (hasError) {
-      setErrors(err);
-      return false;
-    } else {
-      setErrors({});
-    }
-    return form;
-  };
-  const { mutate: resetPassword } = useMutation({
+
+  const form = useForm({
+    resolver: zodResolver(resetPasswordFormSchema),
+    defaultValues: {
+      password: "",
+      confirm_password: "",
+    },
+  });
+
+  const { mutate: resetPassword, isPending: resettingPassword } = useMutation({
     mutationFn: mutate(routes.resetPassword),
     onSuccess: () => {
       localStorage.removeItem(LocalStorageKeys.accessToken);
       toast.success(t("password_reset_success"));
       navigate("/login");
     },
-    onError: (error) => {
-      if (error.cause) {
-        setErrors(error.cause);
-      }
-    },
   });
 
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-    const valid = validateData();
-    if (valid) {
-      valid.token = props.token;
-      resetPassword(valid);
-    }
+  const onSubmit = (values: z.infer<typeof resetPasswordFormSchema>) => {
+    resetPassword({
+      password: values.password,
+      confirm: values.confirm_password,
+      token: token,
+    });
   };
 
-  const { isError } = useQuery({
-    queryKey: ["checkResetToken", { token: props.token }],
-    queryFn: query(routes.checkResetToken, { body: { token: props.token } }),
-    enabled: !!props.token,
+  const [isPasswordFieldFocused, setIsPasswordFieldFocused] = useState(false);
+
+  const { isError, isLoading } = useQuery({
+    queryKey: ["checkResetToken", { token: token }],
+    queryFn: query(routes.checkResetToken, { body: { token: token } }),
+    enabled: !!token,
   });
+
+  if (isLoading) return <Loading />;
 
   if (isError) navigate("/invalid-reset");
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <form
-        className="w-full max-w-md mx-auto rounded-lg bg-white shadow-lg p-6"
-        onSubmit={(e) => {
-          handleSubmit(e);
-        }}
-      >
-        <div className="py-4 text-center text-xl font-bold">
-          {t("reset_password")}
-        </div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-full max-w-md mx-auto rounded-lg bg-white shadow-lg p-6"
+        >
+          <div className="py-4 text-center text-xl font-bold">
+            {t("reset_password")}
+          </div>
 
-        <div className="space-y-6">
-          <div>
-            <PasswordInput
+          <div className="space-y-3">
+            <FormField
+              control={form.control}
               name="password"
-              placeholder={t("new_password")}
-              onChange={handleChange}
-              onFocus={() => setIsPasswordFieldFocused(true)}
-              onBlur={() => setIsPasswordFieldFocused(false)}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel aria-required>{t("new_password")}</FormLabel>
+                  <FormControl>
+                    <PasswordInput
+                      {...field}
+                      placeholder={t("new_password")}
+                      onFocus={() => setIsPasswordFieldFocused(true)}
+                      onBlur={() => setIsPasswordFieldFocused(false)}
+                    />
+                  </FormControl>
+                  <div
+                    className={cn(
+                      "text-small pl-2 text-secondary-500",
+                      !isPasswordFieldFocused && "hidden",
+                    )}
+                    aria-live="polite"
+                  >
+                    <ValidationHelper
+                      isInputEmpty={!field.value}
+                      successMessage={t("password_success_message")}
+                      validations={[
+                        {
+                          description: "password_length_validation",
+                          fulfilled: (field.value || "").length >= 8,
+                        },
+                        {
+                          description: "password_lowercase_validation",
+                          fulfilled: /[a-z]/.test(field.value || ""),
+                        },
+                        {
+                          description: "password_uppercase_validation",
+                          fulfilled: /[A-Z]/.test(field.value || ""),
+                        },
+                        {
+                          description: "password_number_validation",
+                          fulfilled: /\d/.test(field.value || ""),
+                        },
+                      ]}
+                    />
+                  </div>
+
+                  <div className={cn(isPasswordFieldFocused && "hidden")}>
+                    <FormMessage />
+                  </div>
+                </FormItem>
+              )}
             />
-            {errors.password && (
-              <div className="mt-1 text-red-500 text-xs" data-input-error>
-                {errors.password}
-              </div>
-            )}
-            {isPasswordFieldFocused && (
-              <div
-                className="text-small mt-2 pl-2 text-secondary-500"
-                aria-live="polite"
-              >
-                <ValidationHelper
-                  isInputEmpty={!form.password}
-                  successMessage={t("password_success_message")}
-                  validations={[
-                    {
-                      description: "password_length_validation",
-                      fulfilled: form.password?.length >= 8,
-                    },
-                    {
-                      description: "password_lowercase_validation",
-                      fulfilled: /[a-z]/.test(form.password),
-                    },
-                    {
-                      description: "password_uppercase_validation",
-                      fulfilled: /[A-Z]/.test(form.password),
-                    },
-                    {
-                      description: "password_number_validation",
-                      fulfilled: /\d/.test(form.password),
-                    },
-                  ]}
-                />
-              </div>
-            )}
+
+            <FormField
+              control={form.control}
+              name="confirm_password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel aria-required>{t("confirm_password")}</FormLabel>
+                  <FormControl>
+                    <PasswordInput
+                      {...field}
+                      placeholder={t("confirm_password")}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
           </div>
 
-          <div>
-            <PasswordInput
-              name="confirm"
-              placeholder={t("confirm_password")}
-              onChange={handleChange}
-            />
-            {errors.confirm && (
-              <div className="mt-1 text-red-500 text-xs" data-input-error>
-                {errors.confirm}
-              </div>
-            )}
+          <div className="flex flex-wrap justify-between mt-4 gap-4">
+            <Button
+              variant="outline"
+              type="button"
+              disabled={resettingPassword}
+              onClick={() => navigate("/login")}
+              className="w-full sm:w-auto"
+            >
+              {t("cancel")}
+            </Button>
+            <Button
+              variant="primary"
+              type="submit"
+              disabled={resettingPassword}
+              className="w-full sm:w-auto"
+            >
+              {t("reset")}
+            </Button>
           </div>
-        </div>
-
-        <div className="grid p-4 sm:flex sm:justify-between gap-4 mt-6">
-          <Button
-            variant="outline"
-            type="button"
-            onClick={() => navigate("/login")}
-            className="w-full sm:w-auto"
-          >
-            <span>{t("cancel")}</span>
-          </Button>
-          <Button
-            variant="primary"
-            type="submit"
-            onClick={(e) => handleSubmit(e)}
-            className="w-full sm:w-auto"
-          >
-            <span>{t("reset")}</span>
-          </Button>
-        </div>
-      </form>
+        </form>
+      </Form>
     </div>
   );
-};
-
-export default ResetPassword;
+}
